fbb78d3b5c83c79ac5f7df544dac258125b24a36
Douglas Qi <45590672+qizidog@users.noreply.github.com>
2021-03-03 18:22:46 +0800

    初始化整个谷粒商城项目，创建服务module架构

---
+++ gulimall-member/src/test/java/com/qizi/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qizi.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
